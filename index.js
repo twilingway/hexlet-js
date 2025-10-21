2166e02deb84c574b90c840c289fd279800962bb
glazunov aleksei <993468@gmail.com>
2025-10-21 19:58:44 +0400

    add eslint and prettir rules

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import getBirthYear from "./src/index.js";
+import getBirthYear from './src/index.js';
 
-console.log("Сколько вам лет?");
+console.log('Сколько вам лет?');
 const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
 const birthYear = getBirthYear(age);
 console.log(`Ваш год рождения: ${birthYear}`);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export default (age) => {
+export default (_age) => {
   const birthYear = 2005; // Подсчитываем результат
 
   return birthYear;
