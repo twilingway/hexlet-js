baa6326bd201b2ce9df3dc53f09d47b5712516ed
glazunov aleksei <993468@gmail.com>
2025-10-21 18:57:48 +0400

    add birth function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import _ from "lodash";
+import getBirthYear from "./src/index.js";
 
-console.log("Hello, Hexlet");
-console.log(_.last(["one", "two"]));
+console.log("Сколько вам лет?");
+const age = 18; // Каким-то способом получаем возраст пользователя, через терминал. Сейчас не важно как
+const birthYear = getBirthYear(age);
+console.log(`Ваш год рождения: ${birthYear}`);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export default (age) => {
+  const birthYear = 2005; // Подсчитываем результат
+
+  return birthYear;
+};
